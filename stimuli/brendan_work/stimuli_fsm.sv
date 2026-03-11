4eb7135c2ca13a94e50d6fe98a85a0609c8d1da7
Brendan Hendrata <brendan040305@gmail.com>
2026-03-11 14:54:18 -0400

    Reformated code for readability

--- stimuli/brendan_work/stimuli_fsm.sv
+++ stimuli/brendan_work/stimuli_fsm.sv
@@ -6,50 +6,67 @@ module stimuli_fsm (
     localparam DATA_W = stim_if.DATA_W;
     localparam NUM_CONSTRAINTS = stim_if.NUM_CONSTRAINTS;
 
-    // Bounded LFSR
-    logic lfsr_enable, lfsr_seed_load, lfsr_valid;
+    // Bounded LFSR =================================
+    logic lfsr_enable;
+    logic lfsr_seed_load;
+    logic lfsr_valid;
+
+    logic [DATA_W-1:0] registered_upper_bound;
+    logic [DATA_W-1:0] registered_lower_bound;
     logic [DATA_W-1:0] lfsr_output;
+
     bounded_LFSR #(.W(DATA_W)) lfsr (
-        .clk(stim_if.clk),
-        .rst_n(stim_if.rst_n),
-        .enable(lfsr_enable),
-        .seed(stim_if.seed),
-        .seed_load(lfsr_seed_load),
-        .lo(stim_if.lower_bound),
-        .hi(stim_if.upper_bound),
-        .addr(lfsr_output),
-        .valid(lfsr_valid),
-        .diff()
+        .clk        (stim_if.clk),
+        .rst_n      (stim_if.rst_n),
+        .enable     (lfsr_enable),
+        .seed       (stim_if.seed),
+        .seed_load  (lfsr_seed_load),
+        .lo         (registered_lower_bound),
+        .hi         (registered_upper_bound),
+        .addr       (lfsr_output),
+        .valid      (lfsr_valid),
+        .diff       ()
     );
 
-    // Connect lfsr_output to constraint solvers
+    // Constraint solvers =================================
     logic [DATA_W-1:0] solver_output [NUM_CONSTRAINTS];
+
     // TODO: implement constraint solvers here
     // e.g. constraint_id = 0: odd, 1: even
     assign solver_output[0] = {lfsr_output[DATA_W-1:1], 1'b1};
     assign solver_output[1] = {lfsr_output[DATA_W-1:1], 1'b0};
 
-    // Register seed and bounds when request is valid
+    // Register seed and bounds when request is valid ================
     logic lfsr_req_load;
     logic [$clog2(NUM_CONSTRAINTS)-1:0] registered_constraint_id;
-    logic [DATA_W-1:0] registered_upper_bound, registered_lower_bound;
 
     always_ff @(posedge stim_if.clk) begin
         if (lfsr_req_load) begin
-            registered_constraint_id <= stim_if.constraint_id;
-            registered_upper_bound <= stim_if.upper_bound;
-            registered_lower_bound <= stim_if.lower_bound;
+            registered_constraint_id    <= stim_if.constraint_id;
+            registered_upper_bound      <= stim_if.upper_bound;
+            registered_lower_bound      <= stim_if.lower_bound;
         end
     end
+
     assign stim_if.solved_data = solver_output[registered_constraint_id];
 
-    /* FSM
-       Status points: lfsr_valid
-       Control points: lfsr_enable
-       Inputs: stim_if.req_valid, stim_if.rsp_ready, stim_if.req_seed_load
-       Outputs: stim_if.req_ready, stim_if.rsp_valid */
-    typedef enum logic [1:0] 
-        {WAIT, SEED_LOAD, SOLVED} state_t;
+    // FSM
+    // Status points: lfsr_valid
+    // Control points: lfsr_enable
+    // Inputs: 
+    //      stim_if.req_valid
+    //      stim_if.rsp_ready
+    //      stim_if.req_seed_load
+    // Outputs: 
+    //      stim_if.req_ready
+    //      stim_if.rsp_valid 
+
+    typedef enum logic [1:0] {
+        WAIT, 
+        SEED_LOAD, 
+        SOLVED
+    } state_t;
+
     state_t state, nextState;
 
     always_ff @(posedge stim_if.clk, negedge stim_if.rst_n) begin
@@ -61,8 +78,8 @@ module stimuli_fsm (
 
     always_comb begin
         lfsr_enable = 0;
-        lfsr_seed_load = 0;
-        lfsr_req_load = 0;
+        lfsr_seed_load = 0; // on seed_load, seed is loaded
+        lfsr_req_load = 0; // on req_load, constraint_id and bounds are loaded
         stim_if.req_ready = 0;
         stim_if.rsp_valid = 0;
 
@@ -78,6 +95,7 @@ module stimuli_fsm (
 
             SEED_LOAD: begin
                 stim_if.req_ready = 1'b1;
+                
                 if (stim_if.req_valid) begin
                     lfsr_enable = 1'b1;
                     lfsr_req_load = 1'b1;
