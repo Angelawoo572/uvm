eb18aa1ed4bf46afcbc4c16abb6be375c549beff
Brendan Hendrata <brendan040305@gmail.com>
2026-03-17 22:10:02 -0400

    WIP: Creating script to generate stimuli fsm RTL

--- stimuli/stimuli_fsm/main.py
+++ stimuli/stimuli_fsm/main.py
@@ -1,6 +1,6 @@
-from sympy import re
-
+import re
 import typer
+import subprocess
 from typing import Annotated
 
 from identify import identify_constraint
@@ -8,40 +8,14 @@ from generate import generate_constraint
 
 app = typer.Typer(add_completion=False)
 
-@app.command()
-def one_line(
-    input_filename: Annotated[str, typer.Argument(help="Input SystemVerilog file containing constraints. E.g., 'constraints.sv'")], 
-    output_filename: Annotated[str, typer.Argument(help="Output file for generated constraints")]
-) -> None:
-    try:
-        with open(input_filename, "r") as f:
-            lines = f.readlines()
-    except Exception as e:
-        print(f"Error reading file: {e}")
-        return
-    
-    content = []
-    line = lines[0] if lines else ""
-    # Remove comments
-    line = re.sub(r'//.*', '', line).strip()
-    constraint_type = identify_constraint(line)
-    if not constraint_type:
-        print("No valid constraint found in the first line.")
-        return
-    content.append(generate_constraint(constraint_type, line))
-    
-    try:
-        with open(output_filename, "w") as f:
-            f.write("\n".join(content))
-        print(f"Successfully wrote to {output_filename}")
-    except Exception as e:
-        print(f"Error writing file: {e}")
-
+# Assume input has been sanitized and only contains valid constraints in each line (one per line). 
+# i.e. each line is a constraint or a comment (Comments start with //)
 @app.command()
 def main(
-    input_filename: Annotated[str, typer.Argument(help="Input SystemVerilog file containing constraints. E.g., 'constraints.sv'")], 
-    output_filename: Annotated[str, typer.Argument(help="Output file for generated constraints")]
+    input_filename: Annotated[str, typer.Argument(help="Input text file containing constraints. (Output of parser) E.g., 'constraints.txt',")], 
+    output_directory: Annotated[str, typer.Argument(help="Output directory to save the generated stimuli_fsm RTL. E.g., 'output/'")]
 ) -> None:
+    # Open and read file
     try:
         with open(input_filename, "r") as f:
             lines = f.readlines()
@@ -49,27 +23,33 @@ def main(
         print(f"Error reading file: {e}")
         return
     
+    # Parse file lines and save into content list (removes comments)
     content = []
     for line in lines:
         line = re.sub(r'//.*', '', line).strip() # remove comments
-        constraint_type = identify_constraint(line)
+        constraint_type = identify_constraint(line) # should return str
         if not constraint_type:
             continue # skip lines without constraint
-        content.append(generate_constraint(constraint_type, line))
+        content.append(generate_constraint(constraint_type, line)) # content could be tuple (module name, RTL)
     
     if not content:
         print("No valid constraints found in the file.")
         return
-        
-    try:
-        with open(output_filename, "w") as f:
-            f.write("\n".join(content))
-        print(f"Successfully wrote to {output_filename}")
-    except Exception as e:
-        print(f"Error writing file: {e}")
+    
+    # List of files
+    files_to_copy = [
+        "seq_stim_if.svh",
+        "stimuli_fsm.sv",
+        "bounded_LFSR.sv"
+    ]
+
+    files_to_modify = [
+        "constraint_solver.sv"
+    ]
+
+    
     
 if __name__ == "__main__":
     app()  
     # example:
-    # python main.py one-line constraints.sv constraints_rtl.sv
-    # python main.py main constraint.sv constraint_rtl.sv
+    # python main.py main constraint.txt constraints_solver_ex1.sv
