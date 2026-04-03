f4f51c85041d041cb9020f7645de1fe0afd32c5d
Brendan Hendrata <brendan040305@gmail.com>
2026-04-03 13:07:07 -0400

    Added tasks in seq_fsm

--- stimuli/stimuli_fsm/stimuli_fsm_tb.sv
+++ stimuli/stimuli_fsm/stimuli_fsm_tb.sv
@@ -16,6 +16,10 @@ module stimuli_fsm_tb();
         .stim_if(seq_stim_if_inst)
     );
 
+    seq_fsm seq_fsm_inst (
+        .seq_if(seq_stim_if_inst)
+    );
+
     task automatic reset();
         rst_n <= 0;
         @(posedge clk);
@@ -23,34 +27,34 @@ module stimuli_fsm_tb();
         rst_n <= 1'b1;
     endtask
 
-    task automatic load_seed(bit [31:0] seed);
-        seq_stim_if_inst.req_seed_load <= 1'b1;
-        seq_stim_if_inst.seed <= seed;
-        wait(seq_stim_if_inst.req_ready);
-    endtask
+    // task automatic load_seed(bit [31:0] seed);
+    //     seq_stim_if_inst.req_seed_load <= 1'b1;
+    //     seq_stim_if_inst.seed <= seed;
+    //     wait(seq_stim_if_inst.req_ready);
+    // endtask
 
-    task automatic request_data(
-        bit [$clog2(NUM_CONSTRAINTS)-1:0] constraint_id, 
-        bit [DATA_W-1:0] lower_bound, upper_bound); 
-        wait(seq_stim_if_inst.req_ready);
+    // task automatic request_data(
+    //     bit [$clog2(NUM_CONSTRAINTS)-1:0] constraint_id, 
+    //     bit [DATA_W-1:0] lower_bound, upper_bound); 
+    //     wait(seq_stim_if_inst.req_ready);
 
-        seq_stim_if_inst.req_seed_load <= 1'b0;
-        seq_stim_if_inst.lower_bound <= lower_bound;
-        seq_stim_if_inst.upper_bound <= upper_bound;
-        seq_stim_if_inst.constraint_id <= constraint_id;
-        seq_stim_if_inst.req_valid <= 1'b1;
-        seq_stim_if_inst.rsp_ready <= 1'b1;
+    //     seq_stim_if_inst.req_seed_load <= 1'b0;
+    //     seq_stim_if_inst.lower_bound <= lower_bound;
+    //     seq_stim_if_inst.upper_bound <= upper_bound;
+    //     seq_stim_if_inst.constraint_id <= constraint_id;
+    //     seq_stim_if_inst.req_valid <= 1'b1;
+    //     seq_stim_if_inst.rsp_ready <= 1'b1;
 
-        @(posedge clk);
-        seq_stim_if_inst.req_valid <= 1'b0;
-        seq_stim_if_inst.rsp_ready <= 1'b1;
+    //     @(posedge clk);
+    //     seq_stim_if_inst.req_valid <= 1'b0;
+    //     seq_stim_if_inst.rsp_ready <= 1'b1;
 
-        @(posedge clk);
-        wait(seq_stim_if_inst.rsp_valid==1'b1);
-        @(posedge clk);
+    //     @(posedge clk);
+    //     wait(seq_stim_if_inst.rsp_valid==1'b1);
+    //     @(posedge clk);
 
-        seq_stim_if_inst.rsp_ready <= 1'b0;
-    endtask
+    //     seq_stim_if_inst.rsp_ready <= 1'b0;
+    // endtask
 
     // initial begin
     //     $monitor(
@@ -85,19 +89,19 @@ module stimuli_fsm_tb();
 
     initial begin
         reset();
-        load_seed(32'hF234567);
+        seq_fsm_inst.load_seed(32'hF234567);
         repeat(10) // e.g. inside 32'h56789 and 32'hFAAFFA0
-            request_data(2'b0, 32'h00056789, 32'hFAAFFA0);
+            seq_fsm_inst.request_data(2'b0, 32'h00056789, 32'hFAAFFA0);
         repeat(10) // e.g. addr < 32'hFFFF
-            request_data(2'b0, 32'h0, 32'hFFFF);
+            seq_fsm_inst.request_data(2'b0, 32'h0, 32'hFFFF);
         repeat(10) // e.g. addr > 32'h0000FFFF
-            request_data(2'b0, 32'h0000FFFF, 32'hFFFFFFFF);
+            seq_fsm_inst.request_data(2'b0, 32'h0000FFFF, 32'hFFFFFFFF);
         repeat(20) // e.g. dist
-            request_data(2'b1, 32'h0, 32'hFFFFFFFF);
+            seq_fsm_inst.request_data(2'b1, 32'h0, 32'hFFFFFFFF);
         repeat(10) // e.g. parity
-            request_data(2'd2, 32'h0, 32'hFFFFFFFF);
+            seq_fsm_inst.request_data(2'd2, 32'h0, 32'hFFFFFFFF);
         repeat (10) // e.g. enum
-            request_data(2'd3, 32'h0, 32'hFFFFFFFF);
+            seq_fsm_inst.request_data(2'd3, 32'h0, 32'hFFFFFFFF);
         
         // Display collected results
         begin
