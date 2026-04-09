c5919977c172ea7e069eda1dc737d6a5172928c0
Brendan Hendrata <brendan040305@gmail.com>
2026-04-09 16:34:51 -0400

    Added request data structures

--- stimuli/constraint-example1/seq_fsm.sv
+++ stimuli/constraint-example1/seq_fsm.sv
@@ -11,9 +11,10 @@ module seq_fsm (
     localparam DATA_W = seq_if.DATA_W;
     localparam NUM_CONSTRAINTS = seq_if.NUM_CONSTRAINTS;
 
-    typedef enum logic [1:0] {
+    typedef enum logic [2:0] {
       IDLE,
       RESET,
+      LOAD_SEED,
       REQ_ITEM,
       WAIT_RSP,
       DRIVE
@@ -21,6 +22,108 @@ module seq_fsm (
     state_t state, nextState;
 
     data_to_driver_t data_to_driver [`NUM_SEQUENCES];
+    req_data_t [3:0] req_data [`NUM_SEQUENCES];
+
+    // Sequences
+    // req_data[0]
+    assign req_data[0][0] = {
+      .constraint_id(1),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    assign req_data[0][1] = {
+      .constraint_id(0),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    assign req_data[0][2] = {
+      .constraint_id(0),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    assign req_data[0][3] = {
+      .constraint_id(0),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    // req_data[1]
+    assign req_data[1][0] = {
+      .constraint_id(2),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    assign req_data[1][1] = {
+      .constraint_id(3),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    assign req_data[1][2] = {
+      .constraint_id(4),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    assign req_data[1][3] = {
+      .constraint_id(5),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    // req_data[2]
+    assign req_data[2][0] = {
+      .constraint_id(2),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    assign req_data[2][1] = {
+      .constraint_id(7),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    assign req_data[2][2] = {
+      .constraint_id(4),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    assign req_data[2][3] = {
+      .constraint_id(5),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    // req_data[3]
+    assign req_data[3][0] = {
+      .constraint_id(2),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    assign req_data[3][1] = {
+      .constraint_id(11),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    assign req_data[3][2] = {
+      .constraint_id(12),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
+
+    assign req_data[3][3] = {
+      .constraint_id(13),
+      .lower_bound(0),
+      .upper_bound(32'hFFFF_FFFF)
+    };
 
     task automatic load_seed(bit [31:0] seed);
         seq_if.req_seed_load <= 1'b1;
