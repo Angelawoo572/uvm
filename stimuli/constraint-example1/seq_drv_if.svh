e8db1786d9f42f5afd15e1d735938e1caab89ed1
Brendan Hendrata <brendan040305@gmail.com>
2026-04-09 17:32:03 -0400

    Impelmeented seq<->driver interface

---
+++ stimuli/constraint-example1/seq_drv_if.svh
@@ -0,0 +1,40 @@
+`default_nettype none
+
+interface seq_drv_if (
+    input logic clk,
+    input logic rst_n
+);
+    // Data from seq_fsm to drv
+    data_to_driver_t data_to_driver;
+
+    // Handshake
+    logic req_valid;
+    logic req_ready;
+    logic rsp_valid;
+    logic rsp_ready;
+
+    modport SEQ (
+        input clk,
+        input rst_n,
+
+        output data_to_driver,
+        output req_valid,
+        input req_ready,
+
+        input rsp_valid,
+        output rsp_ready
+    );
+
+    modport DRV (
+        input clk,
+        input rst_n,
+
+        input data_to_driver,
+        input req_valid,
+        output req_ready,
+
+        output rsp_valid,
+        input rsp_ready
+    );
+
+endinterface
